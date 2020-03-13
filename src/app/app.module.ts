36c0a429c3987b85ff45bd5807559455755d8bd8
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-03-12 21:26:04 -0500

    Organizacion de carpetas CONFIG: Se organiza la estructura de carpetas de forma correcta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { JwtModule } from '@auth0/angular-jwt';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -20,6 +21,7 @@ export function tokenGetter() {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter
