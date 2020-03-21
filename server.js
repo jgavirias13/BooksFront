edd37dddbffb3d6f6b18231a7de8ad84e1e68d20
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-03-21 00:35:31 -0500

    heroku bugs

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/Book'));
+app.use(express.static(__dirname+'/dist/books'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/Book/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/books/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
