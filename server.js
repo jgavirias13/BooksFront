688cb85931b1f0566b0e19cd5b41005328c85742
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-03-21 00:41:59 -0500

    Error en server para desplegar en heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/books'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/books/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/Books/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
