c85fe634b9becf0d3d87d688cf317612bb1fc8fc
Juan Pablo Gaviria <jgavirias13@gmail.com>
2020-03-21 00:52:40 -0500

    error al publicar rutas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/books'));
+app.use(express.static(__dirname+'/dist/Books'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/Books/index.html'));
 });
